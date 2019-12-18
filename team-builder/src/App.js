8a0940f24606ed7fc2f5a78cabca8cc52a191f64
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2019-12-18 16:29:31 -0500

    imported useState and setup of state with default value also created Members & MemberForm components

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 import './App.css';
 
 function App() {
+  const [members, setMembers] = useState([
+    {
+      id: 1,
+      name: 'Ronald Brennan',
+      email: 'rbrennan@yada.com',
+      role: ''
+    }
+  ])
+
+
+
   return (
     <div className="App">
       <h1>Hello Members</h1>
